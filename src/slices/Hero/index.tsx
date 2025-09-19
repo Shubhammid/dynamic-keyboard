528ad71d174a8ebac42b84f324df97cf343543c4
Shubham Midgule <shubham.midgule17@gmail.com>
2025-09-19 19:31:00 +0530

    Add Hero slice with associated components, types, and mock data

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      {slice.primary.buy_button_text}
+    </section>
+  );
+};
+
+export default Hero;
