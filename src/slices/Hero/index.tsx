6a83ac2fbf2240fd3d7f49a009b66a83a35c84da
Shubham Midgule <shubham.midgule17@gmail.com>
2025-09-21 08:42:00 +0530

    Added animation to Hero Slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,12 +1,19 @@
-"use client"
+"use client";
 
-
+import { FC, useEffect, useState } from "react";
 import { Content } from "@prismicio/client";
-import { FC } from "react";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { Bounded } from "@/components/Bounded";
 import { Canvas } from "@react-three/fiber";
 import { Scene } from "./Scene";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useProgress } from "@react-three/drei";
+import clsx from "clsx";
+
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger);
 
 /**
  * Props for `Hero`.
@@ -17,6 +24,32 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
+  useGSAP(() => {
+    const mm = gsap.matchMedia();
+
+    mm.add("(prefers-reduced-motion: no-preference)", () => {
+      const split = SplitText.create(".hero-heading", {
+        type: "chars,lines",
+        mask: "lines",
+        linesClass: "line++",
+      });
+
+      const tl = gsap.timeline({ delay: 4.2 });
+
+      tl.from(split.chars, {
+        opacity: 0,
+        y: -120,
+        ease: "back",
+        duration: 0.4,
+        stagger: 0.07,
+      }).to(".hero-body", { opacity: 1, duration: 0.6, ease: "power2.out" });
+    });
+
+    mm.add("(prefers-reduced-motion: reduce)", () => {
+      gsap.set(".hero-heading, .hero-body", { opacity: 1 });
+    });
+  });
+
   return (
     <section
       data-slice-type={slice.slice_type}
@@ -24,7 +57,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       className="blue-gradient-bg relative h-dvh text-white text-shadow-black/30 text-shadow-lg"
     >
       <div className="hero-scene pointer-events-none sticky top-0 h-dvh w-full">
-        <Canvas shadows='soft'>
+        <Canvas shadows="soft">
           <Scene />
         </Canvas>
       </div>
@@ -48,7 +81,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
 
         <Bounded
           fullWidth
-          className="hero-body absolute inset-x-0 bottom-0 md:right-[8vw] md:left-auto"
+          className="hero-body absolute inset-x-0 bottom-0 opacity-0 md:right-[8vw] md:left-auto"
           innerClassName="flex flex-col gap-3"
         >
           <div className="max-w-md">
