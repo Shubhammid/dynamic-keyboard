f0b264a4cbd03b419a4ce7c58a3bc9d321c2090f
Shubham Midgule <shubham.midgule17@gmail.com>
2025-09-24 20:13:33 +0530

    Add Product document type, API integration for checkout, and PurchaseButton functionality

--- src/slices/PurchaseButton/index.tsx
+++ src/slices/PurchaseButton/index.tsx
@@ -13,6 +13,7 @@ import clsx from "clsx";
 import { LuChevronRight, LuLoader } from "react-icons/lu";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { checkout } from "@/checkout";
 
 gsap.registerPlugin(useGSAP);
 
@@ -32,7 +33,7 @@ const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
 
   const handlePurchaseClick = async () => {
     setIsPressed(true);
-    await new Promise((reslove) => setTimeout(reslove, 2000));
+    await checkout();
     setIsPressed(false);
   };
 
